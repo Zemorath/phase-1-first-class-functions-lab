98c708dd996099435af18f628599f5207bbb9bf7
Zemorath <trentward10@gmail.com>
2023-05-05 21:20:07 -0500

    First Class Functions Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (function (drivers) {
+    let newArray = drivers.slice(0, 2)
+    return newArray;
+})
+
+const returnLastTwoDrivers = (function (drivers) {
+    let newArray = drivers.slice(2, 4)
+    return newArray;
+})
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function (fare) {
+        return integer * fare;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, secondFunction) {
+    if (secondFunction === returnFirstTwoDrivers) {
+        return drivers.slice(0, 2)
+    } else {
+        return drivers.slice(2, 4)
+    };
+}
